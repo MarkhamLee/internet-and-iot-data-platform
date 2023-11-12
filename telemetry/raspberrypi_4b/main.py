32ed1aad58721429eb64d20f8f439aa3482101e1
Markham Lee <markhamlee@outlook.com>
2023-11-11 22:12:11 -0800

    monitoring container for Raspberry Pi 4Bs

---
+++ telemetry/raspberrypi_4b/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# Markham Lee (C) 2023
+# https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
+# Primary script for a container that provide hardware monitoring for a
+# Raspberry Pi 4B, tracking CPU: temps, utilization and clock speed, GPU temps
+# and RAM use. The Data transmitted via MQTT so that in a future iteration MQTT
+# messages can be used for remote management of the device in response to
+# issues.
+
+import os
+import json
+import time
+import gc
+import logging
+from rpi4b_data import Rpi4bData
+
+# create logger for logging errors, exceptions and the like
+logging.basicConfig(filename='hardware_data_rpi4b.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s\
+                        : %(message)s')
+
+
+def monitor(client: object, getData: object, topic: str):
+
+    while True:
+
+        time.sleep(1)
+
+        # get CPU utilization
+        cpuUtil = getData.getCPUData()
+
+        # get current RAM use
+        ramUse = getData.getRamData()
+
+        # get current freq and core count
+        cpuFreq, coreCount = getData.getFreq()
+
+        # get CPU temperature
+        cpuTemp = getData.get_rpi4b_temps()
+
+        payload = {
+            "cpuTemp": cpuTemp,
+            "cpuFreq": cpuFreq,
+            "cpuUse": cpuUtil,
+            "ramUse": ramUse
+        }
+
+        payload = json.dumps(payload)
+
+        result = client.publish(topic, payload)
+        status = result[0]
+
+        if status != 0:
+
+            print(f'Failed to send {payload} to: {topic}')
+            logging.debug(f'MQTT publishing failure, return code: {status}')
+
+        del payload, cpuUtil, ramUse, cpuFreq, cpuTemp, status, result
+        gc.collect()
+
+
+def main():
+
+    # instantiate utilities class
+    get_data = Rpi4bData()
+
+    # operating parameters
+    TOPIC = os.environ['TOPIC']
+
+    # load environmental variables
+    MQTT_BROKER = os.environ["MQTT_BROKER"]
+    MQTT_USER = os.environ['MQTT_USER']
+    MQTT_SECRET = os.environ['MQTT_SECRET']
+    MQTT_PORT = int(os.environ['MQTT_PORT'])
+
+    # get unique client ID
+    clientID = get_data.getClientID()
+
+    # get mqtt client
+    client, code = get_data.mqttClient(clientID, MQTT_USER, MQTT_SECRET,
+                                       MQTT_BROKER, MQTT_PORT)
+
+    # start monitoring
+    try:
+        monitor(client, get_data, TOPIC)
+
+    finally:
+        client.loop_stop()
+
+
+if __name__ == '__main__':
+    main()
