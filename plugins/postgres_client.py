98c7f7f7477971343d1165857d3e764f2236199b
Markham Lee <markhamlee@outlook.com>
2023-11-21 22:01:51 -0800

    Added DAG for testing Postgres, streamlined Postgres client

--- plugins/postgres_client.py
+++ plugins/postgres_client.py
@@ -3,19 +3,16 @@
 # https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
 # utilities for writing data to PostgreSQL
 
+import psycopg2
+
 
 class PostgresUtilities():
 
     def __init__(self) -> None:
         pass
 
-    # this is here to support other writing methods or approaches
-    # for now, using the write_df method below this one
-
     @staticmethod
     def postgres_client(params: dict) -> object:
-
-        import psycopg2  # noqa: E402
 
         # connect to DB
 
@@ -27,69 +24,49 @@ class PostgresUtilities():
 
         return conn
 
-    # for instances where we're adding a dataframe to data in an existing
-    # database
+    # strict enforcement of what columns are used ensures data quality
+    # avoids issues where tab delimiting can create erroneous empty columns
+    # in the data frame
     @staticmethod
-    def write_df_postgres(payload: object, TABLE: str, params: dict):
+    def prepare_payload(payload: object, columns: list) -> object:
 
-        import psycopg2  # noqa: E402
         from io import StringIO  # noqa: E402
 
-        # create DB connection
-        try:
-            connection = psycopg2.connect(**params)
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            return error
-
         buffer = StringIO()
-        payload.to_csv(buffer, index_label='id', header=False)
+
+        # explicit column definitions + tab as the delimiter allow us to ingest
+        # text data with punctuation  without having situations where a comma
+        # in a sentence is treated as new column or causes a blank column to be
+        # created.
+        payload.to_csv(buffer, index_label='id', sep='\t', columns=columns,
+                       header=False)
         buffer.seek(0)
 
-        cursor = connection.cursor()
+        return buffer
+
+    @staticmethod
+    def clear_table(connection: object, table: str):
 
         try:
-            cursor.copy_from(buffer, TABLE, sep=",")
+            # clear out table - for things like lists or alerts where we only
+            # want the newest data
+            delete_string = (f'DELETE FROM {table}')
+            cursor = connection.cursor()
+
+            cursor.execute(delete_string)
             connection.commit()
-            cursor.close()
             return 0
 
         except (Exception, psycopg2.DatabaseError) as error:
-            connection.rollback()
-            cursor.close()
             return error
 
-    # for when we only want the most recent data in a database, think:
-    # a list of tasks or items, alerts, current "new things", etc.
     @staticmethod
-    def write_df_postgres_clear(payload: object, TABLE: str, params: dict):
-
-        import psycopg2  # noqa: E402
-        from io import StringIO  # noqa: E402
-
-        # create DB connection
-        try:
-            connection = psycopg2.connect(**params)
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            return error
-
-        buffer = StringIO()
-        payload.to_csv(buffer, index_label='id', header=False)
-        buffer.seek(0)
-        print(buffer)
+    def write_data(connection: object, buffer: object, table: str):
 
         cursor = connection.cursor()
 
-        # clear out table - for things like lists or alerts where we only
-        # want the newest data
-        delete_string = (f'DELETE FROM {TABLE}')
-        cursor.execute(delete_string)
-        connection.commit()
-
-        # write new or replacement data
         try:
-            cursor.copy_from(buffer, TABLE, sep=",")
+            cursor.copy_from(buffer, table, sep="\t")
             connection.commit()
             cursor.close()
             return 0
