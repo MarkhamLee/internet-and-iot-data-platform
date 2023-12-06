a90a145dc83a9eebe903e0c067732a8e3da4e619
Markham Lee <markhamlee@outlook.com>
2023-12-05 20:18:14 -0800

    slight update to write json directly

--- airflow/plugins/influx_client.py
+++ airflow/plugins/influx_client.py
@@ -10,12 +10,11 @@ class InfluxClient():
     @staticmethod
     def influx_client(token, org, url):
 
-        import influxdb_client # noqa E402
+        from influxdb_client import InfluxDBClient # noqa E402
         from influxdb_client.client.write_api import SYNCHRONOUS # noqa E402
 
         # create client
-        write_client = influxdb_client.InfluxDBClient(url=url,
-                                                      token=token, org=org)
+        write_client = InfluxDBClient(url=url, token=token, org=org)
         write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
         return write_api
