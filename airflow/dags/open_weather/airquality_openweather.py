566247ab5d1a6f27b2118ef689c7a39c03bd777c
Markham Lee <markhamlee@outlook.com>
2023-12-05 21:00:23 -0800

    refactor, streamlined InfluxDB write

--- airflow/dags/open_weather/airquality_openweather.py
+++ airflow/dags/open_weather/airquality_openweather.py
@@ -34,12 +34,6 @@ def openweather_air_quality_dag():
     # key for OpenWeather API
     WEATHER_KEY = Variable.get('open_weather')
 
-    # influx DB variables
-    INFLUX_KEY = Variable.get('influx_db_key')
-    ORG = Variable.get('influx_org')
-    URL = Variable.get('influx_url')
-    BUCKET = Variable.get('dashboard_bucket')
-
     @task(retries=1)
     def get_air_quality_data():
 
@@ -72,29 +66,37 @@ def openweather_air_quality_dag():
 
         return utilities.parse_air_data(data)
 
-    @task(retries=2)
+    @task(retries=1)
     def write_data(data: dict):
 
         # Airflow will parse these files every 30s (default) so we move these
         # imports into the functions so that airflow isn't constantly wasting
         # cycles importing libraries.
 
+        # influx DB variables
+        INFLUX_KEY = Variable.get('influx_db_key')
+        ORG = Variable.get('influx_org')
+        URL = Variable.get('influx_url')
+        BUCKET = Variable.get('dashboard_bucket')
+
         from plugins.influx_client import InfluxClient  # noqa: E402
         influx = InfluxClient()
 
-        from influxdb_client import Point  # noqa: E402
-        # get the client for connecting to InfluxDB
         client = influx.influx_client(INFLUX_KEY, ORG, URL)
 
-        # create object for writing to Influx
-        point = (
-            Point("airq")
-            .field("carbon_monoxide", data['co'])
-            .field("pm_2", data['pm2'])
-            .field("pm_10", data['pm10'])
-        )
+        # base payload
+        payload = {
+            "measurement": "airq",
+            "tags": {
+                "OpenWeatherAPI": "Air Quality",
+            }
+        }
 
-        client.write(bucket=BUCKET, org=ORG, record=point)
+        # update the payload with the air quality data
+        payload.update({"fields": data})
+
+        # write data to InfluxDB
+        client.write(bucket=BUCKET, record=payload)
 
     # nesting the methods establishes the hiearchy and creates the tasks
     write_data(parse_data(get_air_quality_data()))
