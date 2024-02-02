88870995b8ea80e37cf9a17b8c44af70d3ee2e05
Markham Lee <markhamlee@outlook.com>
2024-02-02 00:15:05 -0800

    Adding DAG for current weather from the Open Weather API

---
+++ airflow_k8s_pod_operator_dags/open_weather/openweather_api_current_k8s_pod_dag.py
@@ -0,0 +1,64 @@
+# (C) Markham Lee
+# https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
+# Airflow DAG to pull down and run an ETL container that connects to the
+# open weather API, pulls down the data for the current weather and then writes
+# that data to InfluxDB. I.e., this DAG doesn't do the ETL work, it's just
+# uses to run the container that does.
+
+from datetime import datetime, timedelta
+from kubernetes.client import models as k8s
+from airflow import DAG
+from airflow.kubernetes.secret import Secret
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator  # noqa: E501
+
+# define instance specific variables
+env_variables = {"CITY": "seattle",
+                 "LAT": "47.6",
+                 "LONG": "-122.3321"}
+
+# load config maps from Kubernetes
+configmaps = [
+    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name="key-etl-variables"))  # noqa: E501
+]
+
+# load all the required secrets from Kubernetes
+secret_env1 = Secret(deploy_type="env", deploy_target="OPENWEATHER_KEY",
+                     secret="openweather-secret", key="OPEN_WEATHER_SECRET")
+
+secret_env2 = Secret(deploy_type="env", deploy_target="INFLUX_KEY",
+                     secret="influxdb-secret", key="PROD_INFLUX_KEY")
+
+secret_env3 = Secret(deploy_type="env", deploy_target="ALERT_WEBHOOK",
+                     secret="slack-webhook-pipeline-failures",
+                     key="WEBHOOK_ETL_ALERTS")
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 24),
+    'retries': 0
+}
+
+with DAG(
+    dag_id='openweather_current',
+    schedule=timedelta(minutes=15),
+    default_args=default_args,
+    catchup=False,
+) as dag:
+    k = KubernetesPodOperator(
+        namespace='airflow',
+        image_pull_secrets=[k8s.V1LocalObjectReference("dockersecrets")],
+        image="markhamlee/openweather_current:latest",
+        env_vars=env_variables,
+        env_from=configmaps,
+        secrets=[secret_env1, secret_env2, secret_env3],
+        cmds=["python3"],
+        arguments=["main.py"],
+        name="openweather_current_pod",
+        task_id="openweather_current_data",
+        is_delete_operator_pod=True,
+        hostnetwork=False,
+        get_logs=True,
+        log_events_on_failure=True,
+        in_cluster=True,
+        startup_timeout_seconds=180
+    )
