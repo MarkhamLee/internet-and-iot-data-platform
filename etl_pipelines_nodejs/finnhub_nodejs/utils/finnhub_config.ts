ce01300825ed52ddd778dd841c775e11258ece58
Markham Lee <markhamlee@outlook.com>
2024-03-11 16:29:39 -0700

    New folder for Node.js Finnhub ETL to hopefully address GitHub actions issue

---
+++ etl_pipelines_nodejs/finnhub_nodejs/utils/finnhub_config.ts
@@ -0,0 +1,49 @@
+// (C) Markham Lee 2023 - 2024
+// productivity-music-stocks-weather-IoT-dashboard
+// https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
+// Config file for Finnhub ETL
+
+
+// interface for Finnhub data
+export interface VarConfig {
+    bucket: string;
+    finnhubKey: string;
+    measurement: string;
+    org: string; 
+    stock: string;
+    token: string;
+    url: string;
+    webHookUrl: string;
+    
+  };
+
+// For using the Finnhub interface
+export const config: VarConfig = {
+    
+    bucket: process.env.BUCKET as string,
+    finnhubKey: process.env.FINNHUB_SECRET as string,
+    measurement: process.env.FINNHUB_MEASUREMENT_SPY as string,
+    org: process.env.INFLUX_ORG as string,
+    stock: process.env.STOCK_SYMBOL as string,
+    token: process.env.INFLUX_KEY as string,
+    url: process.env.INFLUX_URL as string,
+    webHookUrl: process.env.ALERT_WEBHOOK as string,
+    
+  };
+
+// schema for validating the API response from Finnhub
+export const finnhubSchema = {
+  
+    "type": "object",
+    "properties": {
+        "c": {"type": "number"},
+        "d": {"type": "number"},
+        "dp": {"type": "number"},
+        "h": {"type": "number"},
+        "l": {"type": "number"},
+        "o": {"type": "number"},
+        "pc": {"type": "number"},
+        "t": {"type": "number"},
+    },
+    "required": ["pc", "o", "l", "dp"],
+};
---
+++ etl_pipelines_nodejs/finnhub_nodejs/utils/utilities.ts
@@ -0,0 +1,66 @@
+// (C) Markham Lee 2023 - 2024
+// productivity-music-stocks-weather-IoT-dashboard
+// https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
+// Node variant of the Finnhub Stock Price ETL, pulls down current price data for
+// a given stock and then writes it to InfluxDB.
+
+import axios from 'axios';
+import Ajv from "ajv";
+import {InfluxDB} from '@influxdata/influxdb-client';
+import { config, finnhubSchema } from '../utils/finnhub_config'
+
+
+// create InfluxDB client
+const createInfluxClient = (bucket: string) => {
+
+    const url = config.url
+    const token = config.token
+    const org = config.org
+
+    const client = new InfluxDB({url, token})
+    console.log('InfluxDB client created')
+
+    return client.getWriteApi(org, bucket, 'ns')
+
+    }
+
+const sendSlackAlerts = (message: string) => {
+
+    const payload = JSON.stringify({"text": message})
+        
+        axios.post(config.webHookUrl, payload)
+            .then(function (response) {
+                console.log("Slack message sent successfully with code:", response.status);
+        })
+        
+        .catch(function (error) {
+            console.error("Slack message failure with error: ", error.response.statusText);
+        });
+        
+    }
+
+const validateJson = (data: any) => {
+
+        const ajv = new Ajv()
+    
+        const validData = ajv.validate(finnhubSchema, data)
+    
+        if (validData) {
+    
+            console.log("Data validation successful");
+            return 0
+    
+          } else {
+            
+            const message = "Pipeline failure data validation - OpenWeather Air Quality (nodejs variant), exiting... "
+            console.error("Data validation error: ", ajv.errors);
+            sendSlackAlerts(message)
+            // exit the script so we don't attempt a DB write that won't work or
+            // would write bad data to our db.
+            return 1
+    
+          }
+    
+    }
+
+export {config, createInfluxClient, sendSlackAlerts, validateJson}
