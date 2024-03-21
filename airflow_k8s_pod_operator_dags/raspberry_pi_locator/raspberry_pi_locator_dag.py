d1de9aeb1f94eb91bb6d60b321cf294fce330324
Markham Lee <markhamlee@outlook.com>
2024-03-21 05:15:14 -0700

    Updated DAG for Raspberry Pi Locator

--- airflow_k8s_pod_operator_dags/raspberry_pi_locator/raspberry_pi_locator_dag.py
+++ airflow_k8s_pod_operator_dags/raspberry_pi_locator/raspberry_pi_locator_dag.py
@@ -6,16 +6,14 @@ from datetime import datetime, timedelta
 from kubernetes.client import models as k8s
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.secret import Secret
-from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator  # noqa: E501
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator  # noqa: E501
 
 # define instance specific variables
 env_variables = {"MAX_AGE": "24",
-                 "LOCATOR_URL": 'https://rpilocator.com/feed/?country=US&cat=PI5',  # noqa: E501
+                 "RPI_BASE_URL": "https://rpilocator.com/feed/?country=",  # noqa: E501
+                 "RPI_PRODUCT": "PI5",
+                 "RPI_COUNTRY": "US",
                  "RPI5_TABLE": "rpi_alert_data"}
-
-# load config maps from Kubernetes
-configmaps = [
-    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name="key-etl-variables"))]  # noqa: E501
 
 resource_limits = k8s.V1ResourceRequirements(
             requests={
@@ -28,6 +26,10 @@ resource_limits = k8s.V1ResourceRequirements(
                 },
             )
 
+# load config maps from Kubernetes
+configmaps = [
+    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name="key-etl-variables"))]  # noqa: E501
+
 # load all the required secrets from Kubernetes
 secret_env1 = Secret(deploy_type="env", deploy_target="POSTGRES_USER",
                      secret="postgres-secrets", key="POSTGRES_USER")
@@ -35,9 +37,13 @@ secret_env1 = Secret(deploy_type="env", deploy_target="POSTGRES_USER",
 secret_env2 = Secret(deploy_type="env", deploy_target="POSTGRES_PASSWORD",
                      secret="postgres-secrets", key="POSTGRES_PASSWORD")
 
-secret_env3 = Secret(deploy_type="env", deploy_target="WEBHOOK",
+secret_env3 = Secret(deploy_type="env", deploy_target="PRODUCT_WEBHOOK",
                      secret="slack-webhook-rpi-alerts",
                      key="SLACK_WEBHOOK_RPI5")
+
+secret_env4 = Secret(deploy_type="env", deploy_target="ALERT_WEBHOOK",
+                     secret="slack-webhook-pipeline-failures",
+                     key="WEBHOOK_ETL_ALERTS")
 
 default_args = {
     'owner': 'airflow',
@@ -50,15 +56,17 @@ with DAG(
     schedule=timedelta(hours=1),
     default_args=default_args,
     catchup=False,
+    tags=['k8s-pod-operator-container', 'Python'],
 ) as dag:
     k = KubernetesPodOperator(
         namespace='airflow',
-        container_limits=resource_limits,
+        container_resources=resource_limits,
+        node_selector={'node_type': 'arm64_worker'},
         image_pull_secrets=[k8s.V1LocalObjectReference("dockersecrets")],
-        image="markhamlee/rpi5_stock:latest",
+        image="markhamlee/rpi_stock:latest",
         env_vars=env_variables,
         env_from=configmaps,
-        secrets=[secret_env1, secret_env2, secret_env3],
+        secrets=[secret_env1, secret_env2, secret_env3, secret_env4],
         cmds=["python3"],
         arguments=["main.py"],
         name="raspberry_pi_five_locator",
