1fb4d469541eb3e08f4ee1b39834bd6e7b1e84da
Markham Lee <markhamlee@outlook.com>
2024-03-24 17:32:56 -0700

    re-organized folder, refactored to use common private librarie

---
+++ IoT/dht22_temp_sensor/main.py
@@ -0,0 +1,115 @@
+# Markham Lee 2023
+# Efficient, automated greenhouse
+# https://github.com/MarkhamLee/automated-resource_efficient-greenhouse
+# the script gathers data from a DHT22 temperature sensor and then
+# publishes that data to a MQTT topic.
+import adafruit_dht  
+import board  
+import json
+import time
+import gc
+import os
+import sys
+
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+from iot_libraries.logging_util import logger  # noqa: E402
+from iot_libraries.communications_utilities\
+    import IoTCommunications  # noqa: E402
+
+com_utilities = IoTCommunications()
+
+
+def getTemps(client: object, topic: str, interval: int):
+
+    # The use pulseio parameter is suggested parameter for a Raspberry Pi
+    # may not need to use it with other types of SBCs
+
+    # The sensor fails to read about 1/3 of the time but that can be managed
+    # via exception handling and the next attempt is nearly always successful.
+    # This appears to be an issue of the sensor + the driver, as this rarely
+    # happens on a Raspberry Pi Pico Microcontroller (in my experience). Still,
+    # given that you you can immediately retry reading the data it doesn't
+    # really cause any issues as far as the sensor providing steady/consistent
+    # data.
+
+    temp_device = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+    logger.info('Connected to DHT22')
+
+    while True:
+
+        # get temperature and humidity data
+        try:
+            temp = temp_device.temperature
+            humidity = temp_device.humidity
+
+        except RuntimeError as error:  # noqa: F841
+            logger.debug(f'Device runtime error {error}')
+            continue
+
+        except Exception as error:
+            logger.debug(f'Device read error {error}')
+            continue
+
+        if temp is not None:
+
+            payload = {
+                    "t": temp,
+                    "h": humidity
+                }
+
+            payload = json.dumps(payload)
+            send_message(client, payload, topic)
+            del temp, humidity, payload
+            gc.collect()
+
+        time.sleep(interval)
+
+
+def send_message(client: object, payload: dict, topic: str):
+
+    try:
+        result = client.publish(topic, payload)
+        status = result[0]
+
+    except Exception as error:
+        logger.debug(f'MQTT connection error: {error}\
+                            with status: {status}')
+
+    # given that this is a RAM constrained device,
+    # let's delete everything and do some garbage collection
+
+    del payload, result, status
+    gc.collect()
+
+
+def main():
+
+    # load environmental variables
+    TOPIC = os.environ['TOPIC']
+    INTERVAL = int(os.environ['INTERVAL'])
+    MQTT_BROKER = os.environ['MQTT_BROKER']
+    MQTT_USER = os.environ['MQTT_USER']
+    MQTT_SECRET = os.environ['MQTT_SECRET']
+    MQTT_PORT = int(os.environ['MQTT_PORT'])
+
+    # get unique client ID
+    clientID = com_utilities.getClientID()
+
+    # get mqtt client
+    client, code = com_utilities.mqttClient(clientID, MQTT_USER,
+                                            MQTT_SECRET, MQTT_BROKER,
+                                            MQTT_PORT)
+
+    # start data monitoring
+    try:
+        getTemps(client, TOPIC, INTERVAL)
+
+    finally:
+        client.loop_stop()
+
+
+if __name__ == '__main__':
+    main()
