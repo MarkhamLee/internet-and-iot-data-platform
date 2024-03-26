bbebd12064ccc7a74fd3bab4f5d3c6cef324fd4b
Markham Lee <markhamlee@outlook.com>
2024-03-25 23:12:54 -0700

    Adding query for creating table for yield curve plot

---
+++ etl_pipelines/yield_curve_data/db_files/create_yield_curve_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS yield_curve_plot (
+    "bond_maturity" text,
+    "current_today" numeric,
+    "six_months_ago" numeric,
+    "year ago" numeric,
+    "baseline" numeric
+)
