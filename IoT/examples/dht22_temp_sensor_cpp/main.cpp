0ce009da56910faddb6410b4727c5f5c5e133ccd
Markham Lee <markhamlee@outlook.com>
2024-04-08 23:58:08 -0700

    Very simple c++ sketch for pulling temp data with an ESP32 and a DHT22 temp sensor

---
+++ IoT/examples/dht22_temp_sensor_cpp/main.cpp
@@ -0,0 +1,62 @@
+/*
+* Pulls data from a dht22 temperature sensor
+create a JSON payload and then outputs it to console.
+Target device is an ESP32 - used PlatformIO to 
+deploy the code.
+*/
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHT_SENSOR_PIN 21 
+#define DHT_SENSOR_TYPE DHT22
+#include <ArduinoJson.h>
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+void setup() {  
+
+
+    // DHT22 Sensor Setup 
+    Serial.begin(9600);
+    dht_sensor.begin(); 
+
+    // MQTT setup
+
+}
+
+
+void loop() {
+
+
+   // read humidity
+  float humi  = dht_sensor.readHumidity();
+  // read temperature in Celsius
+  float tempC = dht_sensor.readTemperature();
+
+  // check whether the reading is successful or not
+  if ( isnan(tempC) || isnan(humi)) {
+    Serial.println("Failed to read from DHT sensor!");
+    
+    // TODO: add logic for sending alerts for device read failures
+    // plan is to send them via MQTT to a service on the k8s cluster
+    // that would then send out the Slack messages.
+
+  } else {
+
+    // build JSON message for MQTT 
+
+    JsonDocument payload; // define json document 
+
+    //Add data to the JSON document 
+    payload["temperature"] = tempC;
+    payload["humidity"] = humi;
+
+    serializeJsonPretty(payload, Serial);
+    Serial.println();
+
+  }
+
+  // sleep interval of 30 seconds
+  delay(30000);
+ 
+}
