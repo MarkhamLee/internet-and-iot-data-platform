5ea6fcb0b70933b286de7dbecd36b13ee3d3d9f0
Markham Lee <markhamlee@outlook.com>
2024-04-28 18:21:26 -0700

    Example script for Plantower S5003 Air Quality Sensor

---
+++ IoT/examples/pms5003/main.py
@@ -0,0 +1,86 @@
+# Markham Lee (C) 2023 - 2024
+# Finance, Productivity, IoT Dashboard
+# https://github.com/MarkhamLee/productivity-music-stocks-weather-IoT-dashboard
+# Python script for pulling data from a PMS5003 air quality sensor
+# connected by USB, and then pushing the data to console.
+# Influenced by R. Smith's script for the same sensor:
+# https://github.com/rsmith-nl/ft232-pms5003/tree/main
+import serial
+import logging
+import struct
+from time import sleep
+from sys import stdout
+
+logger = logging.getLogger('asana_etl')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')  # noqa: E501
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# define write
+# based on R. F. Smith's deep dive into the Plantower Manual
+# https://github.com/rsmith-nl/ft232-pms5003/blob/main/dust-monitor.py
+PASSIVE_READ = b"\x42\x4d\xe2\x00\x00\x01\x71"
+PASSIVE_MODE = b"\x42\x4d\xe1\x00\x00\x01\x70"
+ACTIVE_MODE = b"\x42\x4d\xe1\x00\x01\x01\x71"
+SLEEP = b"\x42\x4d\xe4\x00\x00\x01\x73"
+WAKEUP = b"\x42\x4d\xe4\x00\x01\x01\x74"
+
+
+USB = '/dev/ttyUSB0'
+ser = serial.Serial(USB, baudrate=9600, stopbits=1, parity="N",  timeout=2)
+# ser.write([66, 77, 225, 0, 0, 1, 112])  # puts device in passive mode
+ser.write(PASSIVE_MODE)  # puts device in passive mode
+
+
+def get_air_data(interval: int):
+
+    while True:
+
+        # ser.write([0x42, 0x4D, 0xE1, 0x00, 0x01, 0x01, 0x71])
+
+        ser.flushInput()
+        ser.write([66, 77, 226, 0, 0, 1, 113])  # query device for data
+
+        try:
+            air_data = ser.read(32)
+
+            if len(air_data) != 32:
+                continue
+
+            if not air_data.startswith(b"BM"):
+                continue
+
+            numbers = struct.unpack(">HHHHHHHHHHHHHHHH", air_data)
+            # toss the first five readings to give
+            # the sensor time to settle down - avoid read errors
+            # or large outlier values.
+
+            pm1 = float(numbers[2])
+            pm25 = float(numbers[3])
+            pm10 = float(numbers[4])
+
+            logger.info(f'pm1 levels are {pm1}')
+            logger.info(f'PM2.5 levels are: {pm25}')
+            logger.info(f'PM10 levels are: {pm10}')
+
+            sleep(interval)
+
+        except Exception as e:
+            logger.info(f'Read error: {e}')
+            sleep(5)
+
+
+def main():
+
+    interval = 5
+
+    # start monitoring loop
+    get_air_data(interval)
+
+
+if __name__ == '__main__':
+    main()
