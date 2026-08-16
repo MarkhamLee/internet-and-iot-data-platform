98b32760baa2a7624ba49f96a2b910706b59a42e
Markham Lee <markhamdev@markhamlee.com>
2026-08-15 23:44:56 -0700

    Test file for loading site monitor configs, also tests to make sure that common logging library is coming through properly as it can resolve properly in an IDE but not load at run time if everything is configured properly

--- ai_agents/site_monitor_data_ingestion/test_loader.py
+++ ai_agents/site_monitor_data_ingestion/test_loader.py
@@ -1,4 +1,17 @@
+from __future__ import annotations
+
+import sys
 from watch_config_test import load_watch_file
+from ai_agents.agent_library.logging_util import console_logging
+
+logger = console_logging('test1')
+
+# from common.logging_utils import configure_logger
+
+# logger = configure_logger("test_logger")
+
+sys.exit(1)
+
 
 config_file = 'test_config.yml'
 
