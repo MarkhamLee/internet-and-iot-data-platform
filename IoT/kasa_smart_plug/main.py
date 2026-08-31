c328fcdf07517aac9ddc2a37b34f95ac87a44133
Markham Lee <markhamdev@markhamlee.com>
2026-08-30 23:37:52 -0700

    Converting to platform wide private packages for data persistence, logging and alerting.

--- IoT/kasa_smart_plug/main.py
+++ IoT/kasa_smart_plug/main.py
@@ -5,22 +5,17 @@
 # smart plug and writing the data to InfluxDB
 import asyncio
 import os
-import requests
+# import requests
 import sys
 from kasa.iot import IotPlug
 
+import platform_utils.data_utils as data_utils
+import platform_utils.alert_utils as alert_utils
+from platform_utils.platform_logger import configure_logger
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent_dir)
 
-from iot_libraries.logging_util import console_logging  # noqa: E402
-from iot_libraries.influx_client import InfluxClient  # noqa: E402
-from iot_libraries.communications_utilities\
-    import IoTCommunications  # noqa: E402
+logger = configure_logger('Kasa_smart_plug_monitoring')
 
-logger = console_logging('Kasa_smart_plug_monitoring')
-
-com_utilities = IoTCommunications
 
 # Load environmental variables
 BUCKET = os.environ['BUCKET']
@@ -34,9 +29,6 @@ TAG_KEY = os.environ['TAG_KEY']
 TAG_VALUE = os.environ['TAG_VALUE']
 TOKEN = os.environ['TOKEN']
 URL = os.environ['URL']
-UPTIME_KUMA_WEBHOOK = os.environ['UPTIME_KUMA_WEBHOOK']
-
-influxdb_write = InfluxClient()
 
 logger.info('Preparing base InfluxDB payload')
 
@@ -49,7 +41,7 @@ BASE_PAYLOAD = {
     }
 
 # get client
-INFLUX_CLIENT = influxdb_write.influx_client(TOKEN, ORG, URL)
+INFLUX_CLIENT = data_utils.influx_client(TOKEN, ORG, URL)
 
 
 async def get_plug_data(dev):
@@ -59,9 +51,6 @@ async def get_plug_data(dev):
         # poll device for update
         try:
             await dev.update()
-
-            # send out heartbeat
-            send_uptime_kuma_heartbeat()
 
             # write data to InfluxDB
             write_data(dev)
@@ -75,17 +64,6 @@ async def get_plug_data(dev):
         await asyncio.sleep(SLEEP_INTERVAL)  # Sleep some time between updates
 
 
-def send_uptime_kuma_heartbeat():
-
-    # TODO: check response to verify that response
-    # is proper, if not trigger alert
-    try:
-        requests.get(UPTIME_KUMA_WEBHOOK)
-
-    except Exception as e:
-        logger.exception(f'Publishing of Uptime Kuma heartbeat for {DEVICE_ID} failed with error: {e}')  # noqa: E501
-
-
 def write_data(device_data_object):
 
     # parse payload
@@ -97,12 +75,11 @@ def write_data(device_data_object):
         }
 
     try:
-
         # write data to InfluxDB
-        influxdb_write.write_influx_data(INFLUX_CLIENT,
-                                         BASE_PAYLOAD,
-                                         payload,
-                                         BUCKET)
+        data_utils.write_influx_data(INFLUX_CLIENT,
+                                     BASE_PAYLOAD,
+                                     payload,
+                                     BUCKET)
 
     except Exception as e:
         logger.exception('InfluxDB write failed with error: %s',
@@ -114,7 +91,6 @@ def main():
     # connect to device
     logger.info(f'Connecting to device: {DEVICE_ID}')
 
-    # TODO: write better reconnection logic
     try:
         device = IotPlug(DEVICE_IP)
         logger.info('Connected to Kasa Smart Plug, device ID: %s, starting monitoring....',  # noqa: E501
@@ -123,9 +99,9 @@ def main():
     except Exception as e:
         message = (f'Failed to connect to device ID: {DEVICE_ID} at {DEVICE_IP} with error: {e}')  # noqa: E501
         logger.exception(message)
-        com_utilities.send_slack_webhook(DEVICE_ALERT_WEBHOOK,
-                                         message)
-        sys.exit()
+        alert_utils.send_slack_webhook_basic(DEVICE_ALERT_WEBHOOK,
+                                             message)
+        sys.exit(1)
 
     # start device monitoring
     asyncio.run(get_plug_data(device))
